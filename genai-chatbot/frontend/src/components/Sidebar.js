2bd8bb15495b4b7bac7011623bc39b0ee229c33b
prasannahex <prasannas4@hexaware.com>
2026-06-03 15:46:13 +0530

    Update Sidebar.js

--- genai-chatbot/frontend/src/components/Sidebar.js
+++ genai-chatbot/frontend/src/components/Sidebar.js
@@ -1,1 +1,26 @@
+import React from "react";
 
+function Sidebar({ conversations, onSelect }) {
+
+  return (
+    <div className="sidebar">
+
+      <h3>Chat History</h3>
+
+      {
+        conversations.map(c => (
+          <div
+            key={c.id}
+            className="chat-item"
+            onClick={() => onSelect(c.id)}
+          >
+            {c.title}
+          </div>
+        ))
+      }
+
+    </div>
+  );
+}
+
+export default Sidebar;
